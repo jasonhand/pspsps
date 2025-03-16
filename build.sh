cc9601b08310162dc14131b4955229609c49ae35
Jason Hand <jasonhand24@gmail.com>
2025-03-16 11:48:56 -0600

    Build bug fixes

--- build.sh
+++ build.sh
@@ -3,8 +3,12 @@
 # This script is used by Netlify to install dependencies for functions
 set -e # Exit immediately if a command exits with a non-zero status
 
-# Skip Go installation - our project doesn't use Go
+# Skip language installations that we don't need
 export NETLIFY_SKIP_GO_INSTALL=true
+
+echo "Environment information:"
+node -v
+npm -v
 
 echo "Installing project dependencies..."
 npm install
@@ -13,6 +17,20 @@ npm install
 echo "Making dotenv available to functions..."
 mkdir -p netlify/functions/node_modules
 cp -r node_modules/dotenv netlify/functions/node_modules/
+cp -r node_modules/axios netlify/functions/node_modules/
+
+# Create minimal package.json if it doesn't exist
+if [ ! -f netlify/functions/package.json ]; then
+  echo "Creating minimal package.json for functions..."
+  echo '{
+    "name": "petfinder-proxy-function",
+    "version": "1.0.0", 
+    "dependencies": {
+      "axios": "^1.6.5",
+      "dotenv": "^16.4.1"
+    }
+  }' > netlify/functions/package.json
+fi
 
 # Install functions dependencies
 echo "Installing function dependencies..."
